06f3304db1c06eb383ff836dba1eca1019260827
Mathieu Girard <mathieu.m.girard@gmail.com>
2020-11-20 14:33:01 -0500

    fix button

--- demo/src/components/Calendar/calendar.js
+++ demo/src/components/Calendar/calendar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 import Tiles from '../Tiles';
+import Form from 'react-bootstrap/Form';
 import Logo from './LOGO4_vive_fitness_toronto_gym_247.png';
 import 'react-calendar/dist/Calendar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -44,13 +45,21 @@ export default class BookingCalendar extends React.Component {
             <div style={{marginLeft: "35%", marginRight: "30%", marginTop: "25px"}}>
                 <Calendar className="shadow mb-5 bg-white rounded" style={{width: "50%", border: "none"}} onChange={this.updateDate} />
             </div>
-            <div className="shadow mb-5 bg-white rounded" style={{marginLeft: "25%", marginRight: "30%", marginTop: "25px"}}>
+            {/* <div className="shadow mb-5 bg-white rounded" style={{marginLeft: "25%", marginRight: "30%", marginTop: "25px"}}>
                 <DropdownButton key="Danger" id='dropdown-variants-Danger' variant="danger" title={this.state.selectedOption} onClick={this.updateOption}>
                     <Dropdown.Item eventKey="General Work Out">General Work Out</Dropdown.Item>
                     <Dropdown.Item eventKey="Cardio/Treadmill">Cardio/Treadmill</Dropdown.Item>
                     <Dropdown.Item eventKey="Yoga Room">Yoga Room</Dropdown.Item>
                 </DropdownButton>
-            </div>
+            </div> */}
+            <Form.Group style={{width: "25%", marginLeft: "35%"}} controlId="formSector">
+                <Form.Control as="select" defaultValue="Select Workout">
+                    <option>Select Workout</option>
+                    <option>General Workout</option>
+                    <option>Cardio/Treadmill</option>
+                    <option>Yogo Room</option>
+                </Form.Control>
+            </Form.Group>
             <div className="shadow mb-5 bg-white rounded" style={{marginLeft: "25%", marginRight: "30%", marginTop: "25px"}}>
                 <Tiles name={this.state.selectedOption}/>
             </div>
